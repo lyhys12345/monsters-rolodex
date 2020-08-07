55c03d7a4607273a001254b3c5a72006989196d9
Shell <631808222@qq.com>
2020-08-07 18:41:47 +0800

    Updates

---
+++ precache-manifest.dbf948193a072f2aa84104e4535d38ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686924f8dcd1dbb98ea1c123f76df61e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f582460c16b5754396fa",
+    "url": "/monsters-rolodex/static/css/main.85cd21c9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f582460c16b5754396fa",
+    "url": "/monsters-rolodex/static/js/main.10443d20.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
